7bd32f9d3e67d30750b1ebdf9a3506941e693a86
brookinssk <59924799+brookinssk@users.noreply.github.com>
2021-05-24 22:35:08 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/******************************************
+* Hello.java
+*******************************************
+* Sierra Brookins
+* 05/24/2021
+* 255 C01
+********************************************/
+
+public class Hello{
+	
+	public static void main(String[] args){
+		
+		System.out.println("<Sierra Brookins>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Lab 1 project>");
+		System.out.println("<05/24/2021>");
+		System.out.println("<255 C01>");
+		System.out.println();
+	}
+}
